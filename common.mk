4368a6e23a3edcf747e02ef09b9ea369a2a850e3
Jeungwoo Yoo <caisonwoo@gmail.com>
2018-03-27 23:05:23 +0200

    Modified mistyped path of root

--- common.mk
+++ common.mk
@@ -4,13 +4,13 @@
 
 SHELL := bash
 BASH ?= bash
-ROOT ?= $(shell pwd)/
+ROOT ?= $(shell pwd)
 
 
 #
 # Path of each projects
 #
 
-QEMU_PATH		?= $(ROOt)/qemu
+QEMU_PATH		?= $(ROOT)/qemu
 BAREMETAL_PATH	?= $(ROOT)/aarch64-bare-metal-qemu
 TOOLCHAIN_PATH	?= $(ROOT)/toolchains
